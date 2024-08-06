d8ef03b77f0deabba3a72a0af81e3bef6459dd86
Kalyanam Pavan Kumar <144805268+pavankumar4404@users.noreply.github.com>
2024-08-06 18:10:19 +0530

    Time: 65 ms (89.01%), Space: 24.9 MB (64.48%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int minimumPushes(string word) {
         // unordered_map<int,int> mp;
-        vector<int>freq(26,0);
-        int n=word.length();
-        for(int i=0;i<n;i++){
-            freq[(word[i]-'a')]++;
+        vector<int> freq(26, 0);
+        int n = word.length();
+        for (int i = 0; i < n; i++) {
+            freq[(word[i] - 'a')]++;
         }
-        sort(freq.begin(),freq.end(),greater<int>());
-        
-        long long ans=0;
-        int cnt=1;
-        for(int i=0;i<26;i+=8){
-            for(int j=i;(j<i+8 && j<26);j++){
-                ans+=(freq[j]*cnt);
-            }
-            cnt++;
+        sort(freq.begin(), freq.end(), greater<int>());
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if(freq[i] == 0) break;
+            ans += (freq[i] * (i/8 + 1));
         }
         return ans;
     }
