a54a41c8879631df08e99611f80e11bf08da0ebf
Kalyanam Pavan Kumar <144805268+pavankumar4404@users.noreply.github.com>
2024-08-09 21:33:23 +0530

    Time: 10 ms (83.31%), Space: 12.7 MB (97.4%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -15,17 +15,43 @@ public:
         // vector<vector<int>> dp(n, vector<int>(n+1, -1));
         // return lis(0, -1, arr, dp);
 
-        vector<int> dp(n+1, 0), cur(n+1, 0);
-        for(int i=n-1; i>=0; i--){
-            for(int previ = i-1; previ >= -1; previ--){
-                int maxi = dp[previ + 1];
-                if(previ == -1 || arr[i] > arr[previ]){
-                    maxi = max(maxi, 1 + dp[i+1]);
-                }
-                cur[previ + 1] = maxi;
+        // vector<int> dp(n+1, 0), cur(n+1, 0);
+        // for(int i=n-1; i>=0; i--){
+        //     for(int previ = i-1; previ >= -1; previ--){
+        //         int maxi = dp[previ + 1];
+        //         if(previ == -1 || arr[i] > arr[previ]){
+        //             maxi = max(maxi, 1 + dp[i+1]);
+        //         }
+        //         cur[previ + 1] = maxi;
+        //     }
+        //     dp = cur;
+        // }
+        // return dp[0];
+
+        // vector<int> dp(n, 1);
+        // int lis = 1;
+        // for(int i=0; i<n; i++){
+        //     for(int prev=0; prev<i; prev++){
+        //         if(arr[prev] < arr[i]){
+        //             dp[i] = max(dp[i], 1 + dp[prev]);
+        //         }
+        //     }
+        //     lis = max(lis, dp[i]);
+        // }
+        // return lis;
+
+        // bs
+        vector<int> temp;
+        temp.push_back(arr[0]);
+        for(int i=1; i<n; i++){
+            if(arr[i] > temp.back()){
+                temp.push_back(arr[i]);
             }
-            dp = cur;
+            else{
+                int idx = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+                temp[idx] = arr[i];
+            }
         }
-        return dp[0];
+        return temp.size();
     }
 };
