33d29e397303b44183d34f8fffa08b831f04857c
Kalyanam Pavan Kumar <144805268+pavankumar4404@users.noreply.github.com>
2024-08-12 00:43:56 +0530

    Time: 13 ms (80%), Space: 35.1 MB (90%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int finalPositionOfSnake(int n, vector<string>& commands) {
+        int x = 0, y = 0;
+        for (auto i : commands) {
+            if (i == "UP")
+                x--;
+            if (i == "DOWN")
+                x++;
+            if (i == "LEFT")
+                y--;
+            if (i == "RIGHT")
+                y++;
+        }
+        return x * n + y;
+    }
+};
