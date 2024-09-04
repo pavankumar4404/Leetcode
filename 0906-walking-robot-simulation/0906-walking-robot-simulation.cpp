18dc7da5c824ae9203f53d44bdddbe0794dd9c28
Kalyanam Pavan Kumar <144805268+pavankumar4404@users.noreply.github.com>
2024-09-04 17:18:44 +0530

    Time: 71 ms (68.23%), Space: 39.5 MB (65.75%) - LeetHub

--- 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        unordered_map<string,int>mp;
-        for(auto it:obstacles){
-            string key = to_string(it[0])+"+"+to_string(it[1]);
-            mp[key]++;
-        }
-        vector<vector<int>>direc = {{-1,0},{0,1},{1,0},{0,-1}}; 
-        // according to order W N E S
-
-        int dir = 1; // i.e. north 
-        // 0: west, 1: north, 2: east, 3: south
-        int x=0,y=0;
-        int mxDist=0;
-        for(int it:commands){
-            if(it==-2){
-                // move left
-                if(dir==0) dir = 3;
-                else dir--;
-            }
-            else if(it==-1){
-                // move right
-                dir = (dir+1)%4;
-            }
-            else{
-                int k = it;
-                for(int i=1; i<=k; i++){
-                    int newX = x+direc[dir][0];
-                    int newY = y+direc[dir][1];
-                    
-                    string key = to_string(newX)+"+"+to_string(newY);
-                    if(mp.find(key)!=mp.end()){
-                        // obstacle has been found so don't go there
-                        break;
+        set<pair<int,int>> st;
+        for(auto obstacle : obstacles) 
+            st.insert({obstacle[0], obstacle[1]});
+        // 0 -> north, 1 -> east, 2 -> south, 3 -> west
+        int dir = 0, posx = 0, posy = 0, ans = 0;
+        for(auto command : commands) {
+            if(command == -1) {
+                dir = (dir + 1)%4;
+            } else if (command == -2) {
+                dir = (dir + 4 - 1)%4;
+            } else {
+                if(dir == 0) {
+                    for(int i = 1; i <= command; i++) {
+                        if(st.find({posx, posy + 1}) != st.end()) break;
+                        posy++;
                     }
-                    x = newX, y=newY;
-                    mxDist = max(mxDist, x*x+y*y);
+                } else if (dir == 1) {
+                    for(int i = 1; i <= command; i++) {
+                        if(st.find({posx + 1, posy}) != st.end()) break;
+                        posx++;
+                    }
+                } else if (dir == 2) {
+                    for(int i = 1; i <= command; i++) {
+                        if(st.find({posx, posy - 1}) != st.end()) break;
+                        posy--;
+                    }
+                } else {
+                    for(int i = 1; i <= command; i++) {
+                        if(st.find({posx - 1, posy}) != st.end()) break;
+                        posx--;
+                    }
                 }
             }
-        } 
-
-        return mxDist;
+            ans = max(ans, (posx*posx) + (posy*posy));
+        }
+        return ans;
     }
 };
