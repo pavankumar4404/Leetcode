d1c2c6a54f32293cc2c1cd578c4657108c0a481f
Kalyanam Pavan Kumar <144805268+pavankumar4404@users.noreply.github.com>
2024-09-07 23:27:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> resultsArray(vector<vector<int>>& queries, int k) {
+        priority_queue<int> pq;
+        int n = queries.size();
+        vector<int> ans(n, -1);
+        if(k > n) return ans;
+        for(int i=0; i<n; i++){
+            int d = abs(queries[i][0]) + abs(queries[i][1]);
+            pq.push(d);
+            if(pq.size() > k){
+                pq.pop();
+                ans[i] = pq.top();  
+            }
+            // if(pq.size() == k) 
+             
+        }
+        return ans;
+    }
+};
