185341101f75bf4bf9c5467180a3eb04b242d013
Kalyanam Pavan Kumar <144805268+pavankumar4404@users.noreply.github.com>
2024-10-05 11:34:40 +0530

    Time: 4 ms (94.39%), Space: 20.4 MB (80.71%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int> st;
+        vector<int> ans;
         int n = asteroids.size();
-        for(int i = 0; i < n; i++){
-            if(asteroids[i] < 0){
-                while(st.size() && st.back() > 0 && st.back() < abs(asteroids[i])){
-                    st.pop_back();
+        for(int i=0; i<n; i++){
+            if(asteroids[i] > 0) ans.push_back(asteroids[i]);
+
+            else{    //-ve element
+                while(!ans.empty() && ans.back() < abs(asteroids[i]) && ans.back() > 0){
+                    ans.pop_back();
                 }
-                if(st.size() && abs(asteroids[i]) == st.back()){
-                    st.pop_back();
-                    continue;
+                if(ans.empty() || ans.back() < 0){
+                    ans.push_back(asteroids[i]);
                 }
-                if(st.size() == 0 || st.back() < 0) st.push_back(asteroids[i]);
+                else if(ans.back() == abs(asteroids[i])){
+                    ans.pop_back();
+                }
             }
-            else st.push_back(asteroids[i]);
         }
-        // vector<int> ans;
-        return st;
+        return ans;
     }
 };
